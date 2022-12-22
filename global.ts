45fa32b623aa5791ef9a1da9c4d169c2d11a9086
KudoLayton <lhslhg@gmail.com>
2022-12-22 15:35:33 +0900

    Split settings from main.ts

---
+++ global.ts
@@ -0,0 +1,11 @@
+
+import * as fs from 'fs';
+
+export interface ConfluencePluginSettings {
+	ConfToken: string;
+}
+
+export const DEFAULT_SETTINGS: ConfluencePluginSettings = {
+	ConfToken: ''
+}
+
---
+++ settings.ts
@@ -0,0 +1,31 @@
+import {App, PluginSettingTab, Setting} from 'obsidian';
+import ConfluencePlugin from './main';
+
+export default class ConfluencePluginSettingTab extends PluginSettingTab {
+	plugin: ConfluencePlugin;
+
+	constructor(app: App, plugin: ConfluencePlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h1', {text: 'Obsidian to Confluence Settings'});
+
+		new Setting(containerEl)
+			.setName('Confluence Personal Access Token')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('Enter Token')
+				.setValue(this.plugin.settings.ConfToken)
+				.onChange(async (value) => {
+					console.log('Saved Token');
+					this.plugin.settings.ConfToken = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
