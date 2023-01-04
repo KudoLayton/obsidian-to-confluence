510d51dc48de5d8742361255bf3570fadb8da723
KudoLayton <lhslhg@gmail.com>
2023-01-04 23:43:24 +0900

    add convert wiki to storage feature

--- confluence.ts
+++ confluence.ts
@@ -1,15 +1,47 @@
 import ConfluencePluginSettings from './global'
 import {ChildProcess, spawn} from 'child_process'
-import {requestUrl, RequestUrlResponsePromise} from 'obsidian' 
+import {requestUrl, RequestUrlResponsePromise, Vault, FileSystemAdapter} from 'obsidian' 
+import {request} from './request'
 
 export default class Confluence {
-  requestGetConfluence(setting: ConfluencePluginSettings, urlCommand: string): RequestUrlResponsePromise {
+    pluginPath: string
+    setting: ConfluencePluginSettings
+    constructor(vault: Vault, setting: ConfluencePluginSettings){
+        this.setting = setting;
+
+        const adapter = vault.adapter;
+        if(adapter instanceof FileSystemAdapter){
+            this.pluginPath = adapter.getFullPath(vault.configDir + '/plugins/obsidian-to-confluence')
+        } else {
+            this.pluginPath = "";
+        }
+    }
+
+  requestGetConfluence(urlCommand: string): RequestUrlResponsePromise {
     return requestUrl({
-        url: setting.ConfURL+urlCommand,
+        url: this.setting.ConfURL+urlCommand,
         method: 'GET',
         headers: {
-            "Authorization": `Bearer ${setting.ConfToken}`
+            "Authorization": `Bearer ${this.setting.ConfToken}`
         }
     });
+  }
+
+  requestPostJsonConfluence(urlCommand: string, data: any): Promise<string> {
+      return new Promise<string>((resolve, reject)=>{
+          request({
+              method: 'POST',
+              uri: this.setting.ConfURL + urlCommand,
+              token: this.setting.ConfToken,
+              body: JSON.stringify(data)
+          }, this.pluginPath).then((respond)=>resolve(respond), (err)=>reject(err));
+      });
+  }
+
+  requestConvertConfWiki2Storage(source: string):Promise<string>{
+      return this.requestPostJsonConfluence("rest/api/contentbody/convert/storage", {
+          value: source,
+          representation: "wiki"
+      })
   }
 }
